ab1039f7e677dacbea6c5a5bf057e16fecdbb308
Artem Bykov <bykovaa94@mail.ru>
2024-10-09 13:12:17 +0300

    Setup project for GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: './js/bundle.js',
-    publicPath: '/'
+    publicPath: '/Kata-final-block/'
   },
 
   // Source maps для удобства отладки
